147823986add97af84ccbc649f56a87b6bf8f07b
praivesi <praivesi@gmail.com>
2024-08-25 17:35:00 +0900

    feat: make diesel schema.rs

--- whistle/src/schema.rs
+++ whistle/src/schema.rs
@@ -10,3 +10,38 @@ diesel::table! {
         mtime -> Date,
     }
 }
+
+diesel::table! {
+    create_bp_period_tbl (id) {
+        id -> Nullable<Integer>,
+        user_id -> Integer,
+        parent_period_id -> Integer,
+        name -> Text,
+        desc -> Nullable<Text>,
+        start_date -> Date,
+        max_use_hour -> Integer,
+        d_strategy -> Text,
+        h_strategy -> Integer,
+        used_day -> Integer,
+        used_hour -> Integer,
+        ctime -> Date,
+        mtime -> Date,
+    }
+}
+
+diesel::table! {
+    create_bp_prnp_tbl (id) {
+        id -> Nullable<Integer>,
+        life_id -> Integer,
+        principle -> Text,
+        desc -> Nullable<Text>,
+        ctime -> Date,
+        mtime -> Date,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    cfg_user_tbl,
+    create_bp_period_tbl,
+    create_bp_prnp_tbl,
+);
